622d9ab0f284e608ead3c540b7fe56721d81aa53
rigox <rigoc1994@gmail.com>
2017-10-08 10:56:28 -0400

    i added a dynamic route to the app

---
+++ headlines.py
@@ -0,0 +1,43 @@
+from flask import Flask
+import feedparser
+
+app =  Flask(__name__)
+
+
+RSS_FEED  ={
+     "bbc": "http://feeds.bbci.co.uk/news/rss.xml",
+     'cnn':   'http://rss.cnn.com/rss/edition.rss',
+     'fox':   'http://feeds.foxnews.com/foxnews/latest',
+     'iol':    'http://www.iol.co.za/cmlink/1.640'
+}
+
+@app.route("/")
+@app.route("/<publication>")
+def get_news(publication = 'bbc'):
+
+    feed  = feedparser.parse(RSS_FEED[publication])
+    first_artilce  = feed['entries'][0]
+
+    return """
+          <html>
+
+           <body>
+
+             <h1>HeadLines</h1>
+
+             <b>{0}</b><br  />
+             <i>{1}</i> <br / >
+
+             <p>{2} </p>  <br />
+
+
+           </body>
+
+          </html>
+    """.format(first_artilce.get("title"), first_artilce.get("published"), first_artilce.get("summary"))
+if __name__  == "__main__":
+    app.run(port= 5000  , debug= True)
+
+
+
+
