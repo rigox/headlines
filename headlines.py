c4d7b12249dc40016a9681269a9725271a578d5a
rigox <rigoc1994@gmail.com>
2017-10-17 00:19:14 -0400

    final version

--- headlines.py
+++ headlines.py
@@ -3,6 +3,9 @@ import feedparser
 from  flask import  render_template
 from flask import  request
 import json
+import  datetime
+from flask import  make_response
+
 
 import requests as re
 from sphinx.addnodes import toctree
@@ -59,35 +62,47 @@ def get_news(query):
     return  feed['entries']
 
 
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return  request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
+    return DEFAULTS[key]
+
 
 @app.route("/")
 def home():
- publication = request.args.get("publication")
- if not publication:
-     publication = DEFAULTS['publication']
+ publication =  get_value_with_fallback("publication")
  articles = get_news(publication)
- #### get weather data
- city =  request.args.get('city')
- if not city:
-     city = DEFAULTS['city']
- weather = get_weather(city)
- ## get  currency data a
- currency_from = request.args.get("currency_from")
- if not currency_from:
-     currency_from = DEFAULTS['currency_from']
- currency_to =  request.args.get("currency_to")
- if not currency_to:
-     currency_to = DEFAULTS['currency_to']
- rate , currencies = get_rate(currency_from,currency_to)
- print(currencies)
 
- return  render_template("home.html" , articles = articles,
+ # get weather
+
+ city = get_value_with_fallback("city")
+ weather  = get_weather(city)
+
+ ## get currency
+
+ currency_from = get_value_with_fallback("currency_from")
+ currency_to = get_value_with_fallback("currency_to")
+
+ rate , currencies =  get_rate(currency_from,currency_to)
+
+
+
+
+
+ response =  make_response(render_template("home.html" , articles = articles,
                          weather = weather,
                          currency_from = currency_from , currency_to = currency_to,
                          rate = rate,
                          currencies  = sorted(currencies)
-                         )
-
+                         ))
+ expires  =  datetime.datetime.now() + datetime.timedelta(days=365)
+ response.set_cookie("publication", publication, expires= expires)
+ response.set_cookie("city", city, expires = expires)
+ response.set_cookie("currency_from",currency_from,expires = expires)
+ response.set_cookie("currency_to", currency_to , expires = expires)
+ return  response
 
 
 
